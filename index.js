cb9c4893e47950a16ff5e3413e9f11ef72ed6a22
Rakeshdarda19 <rakeshdarda19@gmail.com>
2022-11-24 19:18:56 +0530

    backend created

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const PORT = 6969
 const { graphqlHTTP } = require("express-graphql")
 const schema = require('./Schemas/index')
+
 
 
 app.use(
